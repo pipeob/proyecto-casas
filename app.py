d0e0554ff97bc423ff2a2a539cc98a686f883e29
Javier <javierfelipeortiz17@gmail.com>
2026-04-30 14:38:18 -0500

    Agrego columns.pkl

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 
 # cargar modelo
 model = joblib.load("modelo_pipeline.pkl")
+columns = joblib.load("columns.pkl")
 
 st.title("🏠 Predicción de precios de casas")
 
@@ -26,21 +27,23 @@ with col2:
 # conversión sqft → m2
 st.markdown(f"📐 Área habitable en m²: **{gr_liv_area * 0.092903:.2f} m²**")
 
+
 # dataframe input
-input_data = pd.DataFrame({
-    "OverallQual": [overall_qual],
-    "GrLivArea": [gr_liv_area],
-    "TotalBsmtSF": [total_bsmt],
-    "GarageCars": [garage_cars],
-    "GarageArea": [garage_area],
-    "YearBuilt": [year_built]
-})
+input_data = pd.DataFrame([{col: None for col in columns}])
+
+# sobrescribir con valores reales
+input_data["OverallQual"] = overall_qual
+input_data["GrLivArea"] = gr_liv_area
+input_data["TotalBsmtSF"] = total_bsmt
+input_data["GarageCars"] = garage_cars
+input_data["GarageArea"] = garage_area
+input_data["YearBuilt"] = year_built
 
 # predicción
 if st.button("Predecir precio"):
     prediction = model.predict(input_data)[0]
 
-    # 💰 rango de precios
+    # rango de precios
     mae = 17726  # usa tu MAE real
     lower = prediction - mae
     upper = prediction + mae
@@ -49,7 +52,7 @@ if st.button("Predecir precio"):
 
     st.info(f"📊 Rango probable: ${lower:,.0f} - ${upper:,.0f}")
 
-    if st.checkbox("Ver importancia de variables"):
+if st.checkbox("Ver importancia de variables"):
         importances = model.named_steps["model"].feature_importances_
         feature_names = model.named_steps["preprocessor"].get_feature_names_out()
 
